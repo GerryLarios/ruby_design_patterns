3e97f9376fc9d62a56011792b21ccbe10e9593fd
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-02-05 09:57:57 -0600

    [REF] report example 2

--- chapter-3/example_1/report.rb
+++ chapter-3/example_1/report.rb
@@ -5,18 +5,36 @@ class Report
     @text = ['Things are going', 'really, really well.']
   end
 
-  def output
-    puts "<html>"
-    puts "\t<head>"
-    puts "\t\t<title>#{@title}</title>"
-    puts "\t<head>"
-    puts "\t<body>"
-    @text.each { |line| puts "\t\t<p>#{line}</p>"}
-    puts "\t</body>"
-    puts "<html>"
+  def output(format)
+    if format == :plain
+      puts "*** #{@title} ***" 
+    elsif format == :html
+      puts "<html>"
+      puts "\t<head>"
+      puts "\t\t<title>#{@title}</title>"
+      puts "\t<head>"
+      puts "\t<body>"
+    else
+      raise "Unknown format: #{format}"
+    end
+
+    @text.each do |line|
+      if format == :plain
+        puts line
+      else
+        puts "\t\t<p>#{line}</p>"
+      end
+    end 
+    
+    if format == :html
+      puts "\t</body>"
+      puts "</html>"
+    end
   end
 
 end
 
 report = Report.new
-report.output
+report.output(:plain)
+report.output(:html)
+report.output(:csv)
