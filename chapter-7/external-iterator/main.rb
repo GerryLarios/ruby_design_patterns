e1be6d02d2408e438e495d7101f58f8cc7715c37
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-02-26 07:43:20 -0600

    [ADD] merge function

--- chapter-7/external-iterator/main.rb
+++ chapter-7/external-iterator/main.rb
@@ -26,3 +26,28 @@ iterator = ArrayIterator.new colors
 while iterator.has_next?
   puts "item #{iterator.next_item}"
 end
+
+def merge(array_1, array_2)
+  merged = []
+  iterator_1 = ArrayIterator.new array_1
+  iterator_2 = ArrayIterator.new array_2
+
+  while(iterator_1.has_next? && iterator_2.has_next?)
+    if iterator_1.item < iterator_2.item
+      merged << iterator_1.next_item
+    else
+      merged << iterator_2.next_item
+    end
+  end
+
+  while iterator_1.has_next?
+    merged << iterator_1.next_item
+  end
+
+  while iterator_2.has_next?
+    merged << iterator_2.next_item
+  end
+  merged
+end
+
+puts merge([1,2,3,4,5], [10,20,30,40,50])
